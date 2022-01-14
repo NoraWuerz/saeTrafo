1cf7fb6b766d9b774c487a01be0d5a1993591a6d
NoraWuerz <nora.wuerz@posteo.de>
2022-01-14 14:30:40 +0100

    Strukturierungen

---
+++ R/estimators.saeTrafo.R
@@ -0,0 +1,149 @@
+#' Presents point, MSE and CV estimates
+#'
+#' Function \code{estimators} is a generic function used to present point and
+#' mean squared error (MSE) estimates and calculated coefficients of variation
+#' (CV).
+#' @param object an object for which point and/or MSE estimates and/or
+#' calculated CV's are desired.
+#' @param indicator optional character vector that selects which indicators
+#' shall be returned.
+#' @param MSE optional logical. If \code{TRUE}, MSE estimates for selected indicators
+#' per domain are added to the data frame of point estimates. Defaults to
+#' \code{FALSE}.
+#' @param CV optional logical. If \code{TRUE}, coefficients of variation for selected
+#' indicators per domain are added to the data frame of point estimates.
+#' Defaults to \code{FALSE}.
+#' @param ... arguments to be passed to or from other methods.
+#' @return
+#' The return of \code{estimators} depends on the class of its argument. The
+#' documentation of particular methods gives detailed information about the
+#' return of that method.
+#' @export
+
+estimators <- function(object, indicator, MSE, CV, ...) UseMethod("estimators")
+
+
+#' Presents point, MSE and/or CV estimates of an emdiObject
+#'
+#' Method \code{estimators.emdi} presents point and MSE estimates for regional
+#' disaggregated indicators. Coefficients of variation are calculated
+#' using these estimators. This method enables to select for which indicators
+#' the estimates shall be returned. The returned object is suitable for
+#' printing with the \code{print.estimators.emdi} method.
+#' @param object an object of type "emdi", representing point and,
+#' if chosen, MSE estimates.
+#' @param indicator optional character vector that selects which indicators
+#' shall be returned: (i) all calculated indicators ("all");
+#' (ii) each indicator name: "Mean", "Quantile_10", "Quantile_25", "Median",
+#' "Quantile_75", "Quantile_90", "Head_Count",
+#' "Poverty_Gap", "Gini", "Quintile_Share" or the function name/s of
+#' "custom_indicator/s"; (iii) groups of indicators: "Quantiles", "Poverty",
+#' "Inequality" or "Custom". If two of these groups are selected, only the first
+#' one is returned. Note, additional custom indicators can be
+#' defined as argument for model-based approaches (see also \code{\link{ebp}})
+#' and do not appear in groups of indicators even though these might belong to
+#' one of the groups. If the \code{model} argument is of type "fh",
+#' indicator can be set to "all", "Direct", FH", or "FH_Bench" (if emdi
+#' object is overwritten by function benchmark). Defaults to "all".
+#' @param MSE optional logical. If \code{TRUE}, MSE estimates for selected indicators
+#' per domain are added to the data frame of point estimates. Defaults to
+#' \code{FALSE}.
+#' @param CV optional logical. If \code{TRUE}, coefficients of variation for selected
+#' indicators per domain are added to the data frame of point estimates.
+#' Defaults to \code{FALSE}.
+#' @param ... other parameters that can be passed to function \code{estimators}.
+#' @return
+#' The return of \code{estimators.emdi} is an object of type "estimators.emdi" with
+#' point and/or MSE estimates and/or calculated CV's per domain obtained from
+#' \code{emdiObject$ind} and, if chosen, \code{emdiObject$MSE}. These objects
+#' contain two elements, one data frame \code{ind} and a character naming the
+#' indicator or indicator group \code{ind_name}.
+#' @details Objects of class "estimators.emdi" have methods for following generic
+#' functions: \code{head} and \code{tail} (for default documentation, see
+#' \code{\link[utils]{head}}),  \code{as.matrix} (for default documentation, see
+#' \code{\link[base]{matrix}}), \code{as.data.frame} (for default documentation,
+#' see \code{\link[base]{as.data.frame}}), \code{subset} (for default
+#' documentation, see \code{\link[base]{subset}}).
+#' @seealso \code{\link{emdiObject}}, \code{\link{direct}}, \code{\link{ebp}},
+#' \code{\link{fh}}
+#' @rdname estimators
+#' @export
+
+estimators.saeTrafo <- function(object, indicator = "all", MSE = FALSE, CV = FALSE, ...) {
+
+  #estimators_check(object = object, indicator = indicator,
+  #                 MSE = MSE, CV = CV)
+
+  # Only point estimates
+  all_ind <- point_emdi(object = object, indicator = indicator)
+  selected <- colnames(all_ind$ind)[-1]
+
+  if ( MSE == TRUE || CV == TRUE ) {
+    all_precisions <- mse_emdi(object = object, indicator = indicator, CV = TRUE)
+    colnames(all_precisions$ind) <- paste0(colnames(all_precisions$ind), "_MSE")
+    colnames(all_precisions$ind_cv) <- paste0(colnames(all_precisions$ind_cv), "_CV")
+    combined <- data.frame(all_ind$ind, all_precisions$ind, all_precisions$ind_cv)
+    endings <- c("","_MSE", "_CV")[c(TRUE,MSE,CV)]
+
+    combined <- combined[,c("Domain",paste0(rep(selected,each = length(endings)),
+                                            endings))]
+  } else {
+    combined <- all_ind$ind
+  }
+
+  estimators_emdi <- list(ind = combined, ind_name = all_ind$ind_name)
+
+  class(estimators_emdi) <- "estimators.emdi"
+
+  return(estimators_emdi)
+}
+
+# Prints estimators.emdi objects
+#' @export
+
+print.estimators.saeTrafo <- function(x,...) {
+  cat(paste0("Indicator/s: ", x$ind_name, "\n"))
+  print(x$ind)
+}
+
+
+# Tail/head functions ----------------------------------------------------------
+
+
+#' @importFrom utils head
+#' @export
+# CV estimators
+
+head.estimators.saeTrafo <- function(x, n = 6L, addrownums = NULL, ...) {
+  head(x$ind, n = n, addrownums = addrownums, ...)
+}
+
+#' @importFrom utils tail
+#' @export
+
+tail.estimators.saeTrafo <- function(x, n = 6L, keepnums = TRUE, addrownums = NULL, ...) {
+  tail(x$ind, n = n, keepnums = keepnums, ...)
+}
+
+
+# Transforms estimators.emdi objects into a matrix object
+#' @export
+
+as.matrix.estimators.saeTrafo <- function(x,...) {
+  as.matrix(x$ind[,-1])
+}
+
+# Transforms estimators.emdi objects into a dataframe object
+#' @export
+
+as.data.frame.estimators.saeTrafo <- function(x,...) {
+  as.data.frame(x$ind, ...)
+}
+
+# Subsets an estimators.emdi object
+#' @export
+
+subset.estimators.saeTrafo <- function(x, ...) {
+  x <- as.data.frame(x)
+  subset(x = x,  ...)
+}
