342120e096257d9628ae8044e0b4351b39324d0d
NoraWuerz <nora.wuerz@posteo.de>
2022-05-30 14:57:49 +0200

    add gpclib to fix maptools error

--- R/map_plot.saeTrafo.R
+++ R/map_plot.saeTrafo.R
@@ -98,6 +98,7 @@
 #' @importFrom reshape2 melt
 #' @importFrom ggplot2 aes geom_polygon facet_wrap fortify coord_equal labs
 #' @importFrom ggplot2 theme element_blank scale_fill_gradient ggplot ggtitle
+#' @import gpclib
 #' @importFrom maptools readShapePoly unionSpatialPolygons
 
 map_plot <- function(object,
